8b624d4ad7e24f135a37c685726d93b7e1b54aa0
Meysam Ameri <maisamflash@gmail.com>
2023-09-08 15:19:16 +0330

    Implement DbContext

---
+++ Cms.DataLayer/Context/CmsContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cms.DataLayer.Context
+{
+    public class CmsContext:DbContext
+    {
+
+        public CmsContext(DbContextOptions<CmsContext> options ):base(options)
+        {
+            
+        }
+
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+    }
+}
--- Cms.Web/Program.cs
+++ Cms.Web/Program.cs
@@ -1,11 +1,24 @@
+using Cms.DataLayer.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+
+
+#region Database Context
+builder.Services.AddDbContext<CmsContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("CmsConnection"));
+});
+#endregion
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
