0b0ac142227bf1a3d33cffeaff7d834ca01ee62c
Meysam Ameri <maisamflash@gmail.com>
2023-09-09 15:13:23 +0330

    Implement register UI

---
+++ Cms.Core/DTOs/RegisterDto.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cms.Core
+{
+    public class RegisterDto
+    {
+
+        [Display(Name = "نام کاربری")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(100, ErrorMessage = "{0} نمیتواند بیستر از {1} باشد")]
+        public string UserName { get; set; }
+
+        [Display(Name = "نام")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(200, ErrorMessage = "{0} نمیتواند بیستر از {1} باشد")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "نام خانوادگی")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(200, ErrorMessage = "{0} نمیتواند بیستر از {1} باشد")]
+        public string LastName { get; set; }
+
+        [Display(Name = "ایمیل")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string Email { get; set; }
+
+        [Display(Name = "رمز عبور")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string Password { get; set; }
+
+        [Display(Name = "تکرار رمز عبور")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string RePassword { get; set; }
+
+        [Display(Name = "آدرس")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(500, ErrorMessage = "{0} نمیتواند بیستر از {1} باشد")]
+        public string Address { get; set; }
+
+        [Display(Name = "شماره تماس")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string Phone { get; set; }
+
+
+        [Display(Name = "نمایه")]
+        public string Avatar { get; set; }
+
+        [Display(Name = "تاریخ تولد")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public DateTime BirthDate { get; set; }
+    }
+}
---
+++ Cms.Web/Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cms.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        [Route("Register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
