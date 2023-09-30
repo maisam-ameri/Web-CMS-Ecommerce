7e1134ad106f9579420b85c2600180fcf00c6404
Meysam Ameri <maisamflash@gmail.com>
2023-09-30 18:12:17 +0330

    Implement course catetory service

---
+++ Cms.Core/Services/Abstractions/ICourseService.cs
@@ -0,0 +1,14 @@
+﻿using Cms.DataLayer.Entities.Course;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cms.Core.Services.Abstractions
+{
+    public interface ICourseService
+    {
+        IEnumerable<CourseCategory> GetCategories{ get;}
+    }
+}
---
+++ Cms.Core/Services/CourseCategoryService.cs
@@ -0,0 +1,22 @@
+﻿using Cms.Core.Services.Abstractions;
+using Cms.DataLayer.Context;
+using Cms.DataLayer.Entities.Course;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cms.Core.Services
+{
+    public class CourseCategoryService : ICourseService
+    {
+        private CmsContext _context;
+
+        public CourseCategoryService(CmsContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<CourseCategory> GetCategories => _context.CourseCategories.ToList();
+    }
+}
