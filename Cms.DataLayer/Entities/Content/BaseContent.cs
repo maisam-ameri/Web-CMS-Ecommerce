b508a6f7df22d58c17e527efa93fc9dce11f8481
Meysam Ameri <maisamflash@gmail.com>
2023-10-17 23:19:08 +0330

    Create Content Entity

---
+++ Cms.DataLayer/Entities/Content/BaseContent.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cms.DataLayer.Entities.Content
+{
+    public class BaseContent
+    {
+        public BaseContent()
+        {
+            
+        }
+
+
+        [Key]
+        public int ContentId { get; set; }
+
+        [Display(Name = "عنوان")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(200, ErrorMessage = "{0} نمیتواند بیستر از {1} باشد")]
+        public string Title { get; set; }
+
+        [Display(Name = "توضیح مختصر")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(500, ErrorMessage = "{0} نمیتواند بیستر از {1} باشد")]
+        public string ShortDescription { get; set; }
+
+        [Display(Name = "متن اصلی")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(2000, ErrorMessage = "{0} نمیتواند بیستر از {1} باشد")]
+        public string MainText { get; set; }
+
+        [Display(Name = "تگ ها")]
+        [MaxLength(500, ErrorMessage = "{0} نمیتواند بیستر از {1} باشد")]
+        public string? Tags { get; set; }
+
+        [Display(Name = "تصویر")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string ImageName { get; set; }
+
+        [Display(Name = "نمایش در منوی اصلی")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public bool ShowInMainMenu { get; set; }
+
+        [Display(Name = "تاریخ انتشار")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public DateTime PublishDate { get; set; }
+
+        [Display(Name = "تاریخ ویرایش")]
+        public DateTime ModifiedDate { get; set; }
+
+        [Display(Name = "وضعیت")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public bool IsPublished { get; set; }
+        
+        public bool IsDeleted { get; set; }
+
+        [Display(Name = "تعداد بازدید")]
+        public int ViewCount { get; set; }
+
+
+
+        public List<ContentCategory>? Categories { get; set; }
+    }
+}
---
+++ Cms.DataLayer/Entities/Content/ContentCategory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cms.DataLayer.Entities.Content
+{
+    public class ContentCategory
+    {
+
+        public ContentCategory()
+        {
+
+        }
+
+        [Key]
+        public int CategoryId { get; set; }
+        [Display(Name = "عنوان")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(200, ErrorMessage = "{0} نمیتواند بیستر از {1} باشد")]
+        public string Title { get; set; }
+
+
+        public List<BaseContent>? Contents { get; set; }
+    }
+}
