fcd33af4330d059f731c46a96f9823d0bdbf5eee
Meysam Ameri <maisamflash@gmail.com>
2023-10-17 23:44:46 +0330

    Implement Content Category Service

---
+++ Cms.Core/Services/Abstractions/IContentService.cs
@@ -0,0 +1,15 @@
+﻿using Cms.DataLayer.Entities.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cms.Core.Services.Abstractions
+{
+    public interface IContentService
+    {
+        List<Category> GetCategories();
+        Category GetCategory(int categoryId);
+    }
+}
---
+++ Cms.Core/Services/ContentService.cs
@@ -0,0 +1,30 @@
+﻿using Cms.Core.Services.Abstractions;
+using Cms.DataLayer.Context;
+using Cms.DataLayer.Entities.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cms.Core.Services
+{
+    public class ContentService : IContentService
+    {
+        private CmsContext _context;
+
+        public ContentService(CmsContext context)
+        {
+            _context = context;
+        }
+        public List<Category> GetCategories()
+        {
+            return _context.ContentCategories.ToList();
+        }
+
+        public Category GetCategory(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
