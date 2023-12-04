c4b49e31f6752493383e3615a10b1b8befa9a9e3
Meysam Ameri <maisamflash@gmail.com>
2023-12-04 14:26:17 +0330

    Implement Discount Index Page

---
+++ Cms.Web/Pages/Admin/Shop/Discount/Index.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Cms.Web.Pages.Admin.Shop.Discount
+{
+    public class IndexModel : PageModel
+    {
+
+        public void OnGet()
+        {
+            
+        }
+    }
+}
